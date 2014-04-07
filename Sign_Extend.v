91b080abe092a74e004cfa802588ea30f0bd8270
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2014-04-07 15:40:30 +0800

    Fix bugs in Sign_Extend.v
    把data_o打成data_i

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -9,22 +9,22 @@ wire	[32-1:0] data_o;
 
 //Sign extended
 assign data_o[16-1:0] = data_i;
-assign data_i[16] = data_i[15];
-assign data_i[17] = data_i[15];
-assign data_i[18] = data_i[15];
-assign data_i[19] = data_i[15];
-assign data_i[20] = data_i[15];
-assign data_i[21] = data_i[15];
-assign data_i[22] = data_i[15];
-assign data_i[23] = data_i[15];
-assign data_i[24] = data_i[15];
-assign data_i[25] = data_i[15];
-assign data_i[26] = data_i[15];
-assign data_i[27] = data_i[15];
-assign data_i[28] = data_i[15];
-assign data_i[29] = data_i[15];
-assign data_i[30] = data_i[15];
-assign data_i[31] = data_i[15];
+assign data_o[16] = data_i[15];
+assign data_o[17] = data_i[15];
+assign data_o[18] = data_i[15];
+assign data_o[19] = data_i[15];
+assign data_o[20] = data_i[15];
+assign data_o[21] = data_i[15];
+assign data_o[22] = data_i[15];
+assign data_o[23] = data_i[15];
+assign data_o[24] = data_i[15];
+assign data_o[25] = data_i[15];
+assign data_o[26] = data_i[15];
+assign data_o[27] = data_i[15];
+assign data_o[28] = data_i[15];
+assign data_o[29] = data_i[15];
+assign data_o[30] = data_i[15];
+assign data_o[31] = data_i[15];
 
 
 endmodule      
