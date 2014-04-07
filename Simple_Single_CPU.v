57dce5a916bae36277268b2b76dcfe09a75266bf
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2014-04-07 15:28:16 +0800

    兜simple_simgle_CPU.v
    用一堆wire把module連接在一起

--- Simple_Single_CPU.v
+++ Simple_Single_CPU.v
@@ -4,101 +4,116 @@ module Simple_Single_CPU( clk_i, rst_n );
 input         clk_i;
 input         rst_n;
 
-//Internal Signles
-
+//Internal Signals
+wire [32-1:0] instruction, writeData, readData1, readData2, ALU_result, Shifter_result;
+wire RegDst, RegWrite, ALUSrc;
+wire [3-1:0] ALUOP;
 
 //modules
+wire [32-1:0] program_now, program_after;
+
 Program_Counter PC(
         .clk_i(clk_i),      
 	    .rst_n(rst_n),     
-	    .pc_in_i() ,   
-	    .pc_out_o() 
+	    .pc_in_i(program_after) ,   
+	    .pc_out_o(program_now) 
 	    );
 	
 Adder Adder1(
-        .src1_i(),     
-	    .src2_i(),
-	    .sum_o()    
+        .src1_i(program_now),     
+	    .src2_i(32'd4),
+	    .sum_o(program_after)    
 	    );
 	
 Instr_Memory IM(
-        .pc_addr_i(),  
-	    .instr_o()    
+        .pc_addr_i(program_now),  
+	    .instr_o(instruction)    
 	    );
 
+wire [5-1:0] writeReg_addr;
+		
 Mux2to1 #(.size(5)) Mux_Write_Reg(
-        .data0_i(),
-        .data1_i(),
-        .select_i(),
-        .data_o()
+        .data0_i(instruction[20:16]),
+        .data1_i(instruction[15:11]),
+        .select_i(RegDst),
+        .data_o(writeReg_addr)
         );	
 		
 Reg_File RF(
         .clk_i(clk_i),      
 	    .rst_n(rst_n) ,     
-        .RSaddr_i() ,  
-        .RTaddr_i() ,  
-        .RDaddr_i() ,  
-        .RDdata_i()  , 
-        .RegWrite_i(),
-        .RSdata_o() ,  
-        .RTdata_o()   
+        .RSaddr_i(instruction[25:21]) ,  
+        .RTaddr_i(instruction[20:16]) ,  
+        .RDaddr_i(writeReg_addr) ,  
+        .RDdata_i(writeData)  , 
+        .RegWrite_i(RegWrite),
+        .RSdata_o(readData1) ,  
+        .RTdata_o(readData2)   
         );
 	
 Decoder Decoder(
-        .instr_op_i(), 
-	    .RegWrite_o(), 
-	    .ALUOp_o(),   
-	    .ALUSrc_o(),   
-	    .RegDst_o()   
+        .instr_op_i(instruction[31:26]), 
+	    .RegWrite_o(RegWrite), 
+	    .ALUOp_o(ALUOP),   
+	    .ALUSrc_o(ALUSrc),   
+	    .RegDst_o(RegDst)   
 		);
 
+wire [5-1:0] ALU_operation;
+wire [2-1:0] FURslt;
+		
 ALU_Ctrl AC(
-        .funct_i(),   
-        .ALUOp_i(),   
-        .ALU_operation_o(),
-		.FURslt_o()
-        );
-	
-Sign_Extend SE(
-        .data_i(),
-        .data_o()
+        .funct_i(instruction[5:0]),   
+        .ALUOp_i(ALUOP),   
+        .ALU_operation_o(ALU_operation),
+		.FURslt_o(FURslt)
         );
 
+wire [32-1:0] instance_signExtend;
+		
+Sign_Extend SE(
+        .data_i(instruction[15:0]),
+        .data_o(instance_signExtend)
+        );
+
+wire [32-1:0] instance_zeroFilled;
+
 Zero_Filled ZF(
-        .data_i(),
-        .data_o()
+        .data_i(instruction[15:0]),
+        .data_o(instance_zeroFilled)
         );
 		
+wire [32-1:0] ALUinp2;
+		
 Mux2to1 #(.size(32)) ALU_src2Src(
-        .data0_i(),
-        .data1_i(),
-        .select_i(),
-        .data_o()
+        .data0_i(readData2),
+        .data1_i(instance_signExtend),
+        .select_i(ALUSrc),
+        .data_o(ALUinp2)
         );	
 		
 ALU ALU(
-		.aluSrc1(),
-	    .aluSrc2(),
-	    .ALU_operation_i(),
-		.result(),
+		.aluSrc1(readData1),
+	    .aluSrc2(ALUinp2),
+	    .ALU_operation_i(ALU_operation),
+		.result(ALU_result),
 		.zero(),
 		.overflow()
 	    );
 		
 Shifter shifter( 
-		.result(), 
-		.leftRight(),
-		.shamt(),
-		.sftSrc() 
+		.result(Shifter_result), 
+		.leftRight(ALU_operation),
+		.shamt(instruction[10:6]),
+		.sftSrc(ALUinp2) 
 		);
 		
 Mux3to1 #(.size(32)) RDdata_Source(
-        .data0_i(),
-        .data1_i(),
-		.data2_i(),
-        .select_i(),
-        .data_o()
+        .data0_i(ALU_result),
+        .data1_i(Shifter_result),
+		.data2_i(instance_zeroFilled),
+        .select_i(FURslt),
+        .data_o(writeData)
         );			
 
 endmodule
